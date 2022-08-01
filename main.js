0d7b813521a27cd7a731fb01ce1b99e08a1f10ae
Seany-mibb <86206802+Seany-mibb@users.noreply.github.com>
2022-07-31 19:53:18 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 function setup()
 {
+    video = createCapture(600, 750);
+    video.position(200, 200)
     canvas = createCanvas(500, 500)
-    canvas.center();
+    canvas.position(1100, 200);
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded()
+{
+    console.log("Model is loaded");
+}
+
+function draw()
+{
+    background("#8ae6ff")
+}
+
+function gotPoses(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else if(results.length > 0)
+    {
+        console.log(results);
+        console.log("d" + results[0].pose.rightWrist.x);
+        console.log("d " + results[0].pose.leftWrist.x);
+    }
 }
